44ae3a67d3d16f0d4afac60f5acfe2a4c19f1d21
OdeToCode <scott@OdeToCode.com>
2011-02-26 17:57:25 -0500

    Completed Building Apps with MVC 3 Module 1 (Introduction)

---
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            var model = new RestaurantReview()
+            {
+                Name = "Tersiguel's",
+                Rating = 9
+            };
+
+            return View(model);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Location = "Maryland, USA";
+
+            return View();
+        }
+    }
+}
