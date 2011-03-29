0417b028eb368dbf5ee284f873c5d9008e1b79ed
OdeToCode <scott@OdeToCode.com>
2011-03-28 22:20:41 -0400

    finished module 6 (ajax)

--- OdeToFood/OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/OdeToFood/Controllers/HomeController.cs
@@ -4,11 +4,52 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Queries;
+using System.Threading;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult JsonSearch(string q)
+        {
+            var restaurants = _db.Restaurants
+                                 .Where(r => r.Name.Contains(q) ||
+                                             String.IsNullOrEmpty(q))
+                                 .Take(10)
+                                 .Select(r => new
+                                 {
+                                     r.Name, r.Address.City, r.Address.Country
+                                 });
+            return Json(restaurants, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult QuickSearch(string term)
+        {
+            var restaurants = _db.Restaurants
+                                 .Where(r => r.Name.Contains(term))
+                                 .Take(10)
+                                 .Select(r => new { label = r.Name });
+            return Json(restaurants, JsonRequestBehavior.AllowGet);
+            
+        }
+
+        public PartialViewResult Search(string q)
+        {
+            var restaurants = _db.Restaurants
+                                 .Where(r => r.Name.Contains(q) || 
+                                             String.IsNullOrEmpty(q))
+                                 .Take(10);
+            return PartialView("_RestaurantSearchResults", restaurants);
+        }
+
+
+        public PartialViewResult LatestReview()
+        {
+            var review = _db.Reviews.FindTheLatest(1).Single();
+            return PartialView("_Review", review);
+        }
+
         public ViewResult Index()
         {
             ViewBag.Message = "Welcome to Ode To Food";            
@@ -21,5 +62,7 @@ namespace OdeToFood.Controllers
 
             return View();
         }
+
+        OdeToFoodDB _db = new OdeToFoodDB();
     }
 }
---
+++ OdeToFood/OdeToFood/Scripts/OdeToFood.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="jquery-1.4.4-vsdoc.js" />
+/// <reference path="jquery-ui.js" />
+/// <reference path="jQuery.tmpl.js" />
+
+$(document).ready(function () {
+
+    $(":input[data-autocomplete]").each(function () {
+        $(this).autocomplete({ source: $(this).attr("data-autocomplete") });
+    });
+    $(":input[data-datepicker]").datepicker();
+
+    $("#searchForm").submit(function () {
+        $.getJSON($(this).attr("action"),  // the url to get JSON from
+                  $(this).serialize(),     // make q=yellow, for example
+                  function (data) {      // what to do with the response
+                    var result = $("#searchTemplate").tmpl(data);
+                    $("#searchResults").empty()
+                               .append(result);
+                }
+        );
+        return false;
+    });
+})
